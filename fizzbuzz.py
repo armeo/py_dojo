644b0892714e0a17a68ee1c566463e0ceff034d8
armeo <armchockaumnuai@gmail.com>
2015-05-18 11:56:59 +0700

    test input 5 shpuld Buzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,4 +2,6 @@ class FizzBuzz(object):
     def print_string(self, input):
         if input % 3 == 0:
             return 'Fizz'
+        elfi input == 5:
+            return 'Buzz'
         return str(input)
