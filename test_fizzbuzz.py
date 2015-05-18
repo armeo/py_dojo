033d67e2aeb4d13b35385bebf39f6cc2b88a9a2d
armeo <armchockaumnuai@gmail.com>
2015-05-18 11:53:15 +0700

    test input 2

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -7,3 +7,8 @@ class TestFizzBuzz(TestCase):
         fizz_buzz = FizzBuzz()
         actual = fizz_buzz.print_string(1)
         self.assertEqual('1', actual)
+
+    def test_input_2_should_return_2(self):
+        fizz_buzz = FizzBuzz()
+        actual = fizz_buzz.print_string(2)
+        self.assertEqual('2', actual)
